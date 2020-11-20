eee461056cc5809668adea1635e5509efa0bbf53
Alex Morehead <alex.morehead@gmail.com>
2020-11-20 17:38:04 -0600

    Fix bash script reference error in sbatch script

--- scripts/cnn-train.sh
+++ scripts/cnn-train.sh
@@ -11,13 +11,12 @@
 ###############################################################
 
 # Local project path #
-# export my_local=/home/"$USER"/Repositories/lab_repos/DNCON2
+export my_local=/home/"$USER"/Repositories/lab_repos/DNCON2
 
 # Remote project path #
-export my_local=/home/"$USER"/data/DNCON2
+# export my_local=/home/"$USER"/data/DNCON2
 
 module load miniconda3
-# source /home/alexm/Repositories/lab_repos/DNCON2/venv/bin/activate
-source /home/acmwhb/data/DNCON2/venv/bin/activate
+source "$my_local"/venv/bin/activate
 
-python3 "$my_local"/scripts/cnn-train.sh
+python "$my_local"/scripts/cnn-train.py
